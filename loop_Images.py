7d980b9cee3b587c143f802b9b514fb7dcc75ce2
Dimi <46501122+DimisCode@users.noreply.github.com>
2020-12-21 10:11:21 +0100

    Delete loop_Images.py

--- loop_Images.py
+++
@@ -1,9 +0,0 @@
-import glob
-from PIL import Image
-
-images = glob.glob("C:\\Users\\Kas\\Desktop\\League overlay\\Overlay\\saved frames\\*.jpg")
-for image in images:
-    with open(image, 'rb') as file:
-        img = Image.open(file)
-        img.show()
-        
